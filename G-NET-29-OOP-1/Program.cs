7bc694c6daa01156022ddb11f18f7acb8054c561
defaultuser0 <defaultuser0@DESKTOP-R58OB0U>
2026-04-05 13:52:32 +0200

    Q1

--- G-NET-29-OOP-1/Program.cs
+++ G-NET-29-OOP-1/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace G_NET_29_OOP_1
+﻿using System;
+using System.Drawing;
+
+namespace G_NET_29_OOP_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Part 01 : 
+            #region Q1 :
+            //class: Reference Type,,is a blueprint.
+            //struct: is a lightweight Value Type.
+            //EX:
+            // Struct Behavior
+            //Point p1;
+            //p1.X = 10;
+            //Point p2 = p1;
+            //p2.X = 99;        //p1.X=10
+
+            //// Class Behavior 
+            //Student s1 = new Student();
+            //s1.Age = 20;
+            //Student s2 = s1; 
+            //s2.Age = 30; //s1.Age = 30;
+            #endregion
         }
     }
 }
