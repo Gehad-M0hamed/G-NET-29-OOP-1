7c7b6bc72db7de5342d0585898ae9040e737412f
defaultuser0 <defaultuser0@DESKTOP-R58OB0U>
2026-04-05 14:17:32 +0200

    Q3

--- G-NET-29-OOP-1/Program.cs
+++ G-NET-29-OOP-1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
 
 namespace G_NET_29_OOP_1
 {
@@ -8,39 +9,48 @@ namespace G_NET_29_OOP_1
     {
         static void Main(string[] args)
         {
-        //Part 01 : 
-        #region Q1 :
-        //class: Reference Type,,is a blueprint.
-        //struct: is a lightweight Value Type.
-        //EX:
-        // Struct Behavior
-        //Point p1;
-        //p1.X = 10;
-        //Point p2 = p1;
-        //p2.X = 99;        //p1.X=10
+            //Part 01 : 
+            #region Q1 :
+            //class: Reference Type,,is a blueprint.
+            //struct: is a lightweight Value Type.
+            //EX:
+            // Struct Behavior
+            //Point p1;
+            //p1.X = 10;
+            //Point p2 = p1;
+            //p2.X = 99;        //p1.X=10
 
-        //// Class Behavior 
-        //Student s1 = new Student();
-        //s1.Age = 20;
-        //Student s2 = s1; 
-        //s2.Age = 30; //s1.Age = 30;
-        #endregion
+            //// Class Behavior 
+            //Student s1 = new Student();
+            //s1.Age = 20;
+            //Student s2 = s1; 
+            //s2.Age = 30; //s1.Age = 30;
+            #endregion
 
-        #region Q2:
-        // Private: accessible within the class at theye registered,
-        // and can not be accessed from outside.This is the default setting for class members.
-        // Pubic:The member have access to any location within the application or other projects they view.
-        //Ex:
-        //class User
-        //{
-        //    public string userName; //Any part of the code can be accessed and modified.
-        //    private string password; // It cannot be viewed or modified from outside the class for protection.
-        //    public void SetPassword(string p)
-        //    {
-        //        password = p;
-        //    }
-        //}
-        #endregion
-    }
+            #region Q2:
+            // Private: accessible within the class at theye registered,
+            // and can not be accessed from outside.This is the default setting for class members.
+            // Pubic:The member have access to any location within the application or other projects they view.
+            //Ex:
+            //class User
+            //{
+            //    public string userName; //Any part of the code can be accessed and modified.
+            //    private string password; // It cannot be viewed or modified from outside the class for protection.
+            //    public void SetPassword(string p)
+            //    {
+            //        password = p;
+            //    }
+            //}
+            #endregion
+
+            #region Q3:
+            //*Create a new project and choose consol app.
+            //*Add Project Reference, and check my Library project.
+            //*At the top of your code, add using followed by lib name.
+            //*make sure the class is Public.
+            //*write the code.
+            //*run the project.
+            #endregion
+        }
     }
 }
